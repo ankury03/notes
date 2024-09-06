58733d1268aa4979db702179f72f3727ad3570b5
ankury03 <120250151+ankury03@users.noreply.github.com>
2024-09-07 00:14:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const notes = document.querySelector(".notes");
+const button = document.querySelector(".btn");
+let notesBox = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notes.innerHTML = localStorage.getItem("notesBox");
+}
+   
+showNotes();
+
+function Storage(){
+    localStorage.setItem("notesBox" , notes.innerHTML);
+}
+
+
+button.addEventListener("click" , () => {
+    let inputbox = document.createElement("p");
+    let img = document.createElement("img");
+    inputbox.className = "input-box";
+    inputbox.setAttribute("contenteditable" , "true");
+    img.src = "images/delete.png";
+    notes.appendChild(inputbox).appendChild(img);
+})
+
+notes.addEventListener("click" , function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        Storage();
+    }
+    else if(e.target.tagName === "P"){
+        notesBox = document.querySelectorAll(".input-box");
+            notesBox.forEach(el => {
+                el.onkeyup = function(){
+                    Storage();
+                }
+            });
+    }
+})
+
+document.addEventListener("keydown" , e => {
+    if(e.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        e.preventDefault();
+    }
+})
+
+
